5ac01a709ebe43d412eaee269fb8a2d0b0411304
angelikrussi <hannamrrussi@gmail.com>
2022-04-11 22:01:33 -0500

    10. For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+
+  const array1 = [];
+  for (const idx of array) {
+    array1.push(`${appendString}${idx}`);
+  }
+
+  return array1;
+}
