6f4104f74096389f0ba2f61e56391b160bacccf7
angelikrussi <hannamrrussi@gmail.com>
2022-04-13 18:01:22 -0500

    task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction){
+    const newArray = [];
+    try {
+        newArray.push(mathFunction());
+    }
+    catch (erro) {
+        newArray.push(erro.toString());
+    }
+    finally {
+        newArray.push('Guardrail was processed');
+    }
+    return newArray;
+}
