df903f82a04dc431bddae0f1a8ea8050f0dec807
angelikrussi <hannamrrussi@gmail.com>
2022-04-20 23:44:33 -0500

    main task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -38,3 +38,41 @@ function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
 console.log(printTeacher('John', 'Doe'));
+
+
+// Class description interface
+interface classInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Constructor description interface
+interface classConstructor {
+  new(firstName: string, lastName: string): classInterface;
+}
+
+// Creating class and constructor through interfaces 
+class StudentClass implements classInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Creating instance of StudentClass
+const studentClass: StudentClass = new StudentClass('Pepito', 'Martinez');
+console.log(studentClass.displayName());
+console.log(studentClass.workOnHomework());
