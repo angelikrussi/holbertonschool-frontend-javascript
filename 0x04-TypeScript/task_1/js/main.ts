873b2693bef4dbbd5e12467de8f27732019159da
angelikrussi <hannamrrussi@gmail.com>
2022-04-20 23:42:53 -0500

    main task2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,3 +24,12 @@ const teacher3: Teacher = {
 };
 console.log(teacher3);
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
